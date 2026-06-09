e665b151d628f4b1c3795f14c54ac00dcd4373be
Guneet Kaur <124431669+gunk7@users.noreply.github.com>
2026-06-09 11:42:56 +0530

    Switching from Nodemailer to Resend for Mail Services

--- Backend/services/nodemailer.js
+++ Backend/services/nodemailer.js
@@ -1,23 +1,14 @@
 const nodemailer = require("nodemailer");
 
 exports.createTransporter = () => {
-  const port = Number(process.env.SMTP_PORT);
-
+  console.log("SMTP config:");
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: port,
-    secure: port === 465,
+    port: process.env.SMTP_PORT,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASS,
     },
   });
-
-  // Add this
-  transporter.verify((error) => {
-    if (error) console.error("Mail transporter error:", error);
-    else console.log("Mail transporter ready");
-  });
-
   return transporter;
 };
--- Backend/utils/mail.js
+++ Backend/utils/mail.js
@@ -1,6 +1,7 @@
-const { createTransporter } = require("../services/nodemailer");
+/* const { createTransporter } = require("../services/nodemailer");
 
 exports.mailSend = async (to, subject, text) => {
+  console.log("mailSend called:", to, subject);
   try {
     const porter = await createTransporter();
     const mailOptions = {
@@ -10,6 +11,24 @@ exports.mailSend = async (to, subject, text) => {
       html: text,
     };
     await porter.sendMail(mailOptions);
+  } catch (error) {
+    console.log("Error in mailSend:", error);
+    throw new Error("Failed to send email: " + error.message);
+  }
+};
+ */
+
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+exports.mailSend = async (to, subject, html) => {
+  try {
+    await resend.emails.send({
+      from: `${process.env.MAIL_APP_NAME} <onboarding@resend.dev>`,
+      to,
+      subject,
+      html,
+    });
   } catch (error) {
     console.log("Error in mailSend:", error);
     throw new Error("Failed to send email: " + error.message);
